369ec38e2052329fc0e917b2f7734b57a3518eab
Samson Mano <saminnx@gmail.com>
2026-02-05 01:17:36 -0500

    initialized the billiard collision simulation

---
+++ XPBD/billiard_collisions_simulation/billiard_collisions_simulation/main_frm.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace billiard_collisions_simulation
+{
+    partial class main_frm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main_frm));
+            this.toolStripStatusLabel_zoom_value = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusFPSLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel_IsRefresh = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newModelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.glControl_main_panel = new OpenTK.GLControl();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripStatusLabel_zoom_value
+            // 
+            this.toolStripStatusLabel_zoom_value.Name = "toolStripStatusLabel_zoom_value";
+            this.toolStripStatusLabel_zoom_value.Size = new System.Drawing.Size(92, 20);
+            this.toolStripStatusLabel_zoom_value.Text = "Zoom: 100%";
+            // 
+            // toolStripStatusFPSLabel
+            // 
+            this.toolStripStatusFPSLabel.Name = "toolStripStatusFPSLabel";
+            this.toolStripStatusFPSLabel.Size = new System.Drawing.Size(47, 20);
+            this.toolStripStatusFPSLabel.Text = "FPS: 0";
+            // 
+            // toolStripStatusLabel_IsRefresh
+            // 
+            this.toolStripStatusLabel_IsRefresh.Name = "toolStripStatusLabel_IsRefresh";
+            this.toolStripStatusLabel_IsRefresh.Size = new System.Drawing.Size(13, 20);
+            this.toolStripStatusLabel_IsRefresh.Text = " ";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel_zoom_value,
+            this.toolStripStatusLabel_IsRefresh,
+            this.toolStripStatusFPSLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 463);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(817, 26);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newModelToolStripMenuItem,
+            this.optionsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newModelToolStripMenuItem
+            // 
+            this.newModelToolStripMenuItem.Name = "newModelToolStripMenuItem";
+            this.newModelToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.newModelToolStripMenuItem.Text = "New Model";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(817, 28);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // glControl_main_panel
+            // 
+            this.glControl_main_panel.BackColor = System.Drawing.Color.Black;
+            this.glControl_main_panel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.glControl_main_panel.Location = new System.Drawing.Point(170, 119);
+            this.glControl_main_panel.Margin = new System.Windows.Forms.Padding(5);
+            this.glControl_main_panel.Name = "glControl_main_panel";
+            this.glControl_main_panel.Size = new System.Drawing.Size(476, 251);
+            this.glControl_main_panel.TabIndex = 4;
+            this.glControl_main_panel.VSync = false;
+            // 
+            // main_frm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 489);
+            this.Controls.Add(this.glControl_main_panel);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.statusStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "main_frm";
+            this.Text = "Billiard Collisions Simulation";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel_zoom_value;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusFPSLabel;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel_IsRefresh;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newModelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private OpenTK.GLControl glControl_main_panel;
+    }
+}
+

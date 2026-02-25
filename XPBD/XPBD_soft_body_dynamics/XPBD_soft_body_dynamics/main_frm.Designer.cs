0ae7c3166a9c249239f0f90daed3648119ad8bc2
Samson Mano <saminnx@gmail.com>
2026-02-25 13:49:00 -0500

    Continue of initialization

--- XPBD/XPBD_soft_body_dynamics/XPBD_soft_body_dynamics/main_frm.Designer.cs
+++ XPBD/XPBD_soft_body_dynamics/XPBD_soft_body_dynamics/main_frm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main_frm));
             this.SuspendLayout();
             // 
             // main_frm
@@ -35,6 +36,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "main_frm";
             this.Text = "XPBD Soft Body Dynamics";
             this.ResumeLayout(false);

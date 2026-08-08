4e1702e5bcc3dfca81b06ef58615e7f99d59d334
Samson Mano <saminnx@gmail.com>
2026-08-07 22:49:49 -0400

    two dof flexible system solver

---
+++ XPBD/spring_mass_sys_visualizer/spring_mass_sys_visualizer/src/model_store/system5_twodofflexible/twodof_flexiblecollisionSolver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spring_mass_sys_visualizer.src.model_store.system5_twodofflexible
+{
+    public class twodof_flexiblecollisionSolver
+    {
+
+        private List<double> fixedend_mass = new List<double>();
+        private List<double> fixedend_stiffness = new List<double>();
+
+        private List<double> freeend_mass = new List<double>();
+        private List<double> freeend_stiffness = new List<double>();
+
+        private double dampratio_zeta = 0.0;
+        private double const_accla0 = 0.0;
+
+
+
+
+    }
+}
